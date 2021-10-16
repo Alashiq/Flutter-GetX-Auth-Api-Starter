dcc6f4f915320685f332f4993caa3c5e495cbd4a
Alashiq <abdulsamia.alashiq@outlook.com>
2021-10-16 16:24:05 +0200

    change in list

--- lib/Modular/Reviews/Models/review_list.dart
+++ lib/Modular/Reviews/Models/review_list.dart
@@ -1,19 +1,34 @@
-
 import 'package:flutterauthgetxstarter/Modular/Reviews/Models/review.dart';
 
-class ReviewListModel {
-  final int? status;  // 200 - 204 - 400 - 404
-  final List<ReviewModel>? items;
+class ReviewListItemModel {
+  final int? id;
+  final String? name;
+  final String? job;
+  final String? photo;
 
-  ReviewListModel({this.status,this.items});
+  ReviewListItemModel({this.id, this.name, this.job, this.photo});
+
+  factory ReviewListItemModel.fromJson(Map<String, dynamic> jsonData) {
+    return ReviewListItemModel(
+      id: jsonData['id'],
+      name: jsonData['name'],
+      job: jsonData['job'],
+      photo: jsonData['photo'],
+    );
+  }
+}
+
+class ReviewListModel {
+  final int? status; // 200 - 204 - 400 - 404
+  final List<ReviewListItemModel>? items;
+
+  ReviewListModel({this.status, this.items});
 
   factory ReviewListModel.fromJson(Map<String, dynamic> jsonData) {
     return ReviewListModel(
         status: jsonData['status'],
-        items:  jsonData['items']
-              .map<ReviewModel>((data) => ReviewModel.fromJson(data))
-              .toList()
-        );
+        items: jsonData['items']
+            .map<ReviewModel>((data) => ReviewModel.fromJson(data))
+            .toList());
   }
 }
-
