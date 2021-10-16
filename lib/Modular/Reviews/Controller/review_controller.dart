2676ab30f197fbb3afeebb6e7f57c9e8cb051888
Alashiq <abdulsamia.alashiq@outlook.com>
2021-10-16 16:36:21 +0200

    make new single model inside list model without statuscode

--- lib/Modular/Reviews/Controller/review_controller.dart
+++ lib/Modular/Reviews/Controller/review_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutterauthgetxstarter/Modular/Auth/Controllers/auth_controller.dart';
 import 'package:flutterauthgetxstarter/Modular/Reviews/Models/review_list.dart';
 import 'package:flutterauthgetxstarter/Modular/Reviews/Services/review_api.dart';
 import 'package:flutterauthgetxstarter/View/Widgets/loading/loading.dart';
@@ -5,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 class ReviewController extends GetxController {
+  AuthController authController = Get.find();
   ReviewListModel? reviews;
   final box = GetStorage();
 
@@ -25,14 +27,10 @@ class ReviewController extends GetxController {
       homeScreen = true;
       update();
     } else if (reviews!.status == 401) {
-      print("expired token 401nn");
-      box.remove("token");
-      Get.offAndToNamed("/login");
+      authController.logout();
     } else {
       print("someting wrong 400");
-      box.remove("token");
-      Get.offAndToNamed("/login");
+      authController.logout();
     }
   }
 }
-
--- lib/Modular/Reviews/Models/review_list.dart
+++ lib/Modular/Reviews/Models/review_list.dart
@@ -1,34 +1,39 @@
-import 'package:flutterauthgetxstarter/Modular/Reviews/Models/review.dart';
 
-class ReviewListItemModel {
+
+class ReviewModel {
   final int? id;
   final String? name;
   final String? job;
   final String? photo;
 
-  ReviewListItemModel({this.id, this.name, this.job, this.photo});
 
-  factory ReviewListItemModel.fromJson(Map<String, dynamic> jsonData) {
-    return ReviewListItemModel(
-      id: jsonData['id'],
-      name: jsonData['name'],
-      job: jsonData['job'],
-      photo: jsonData['photo'],
-    );
+  ReviewModel({this.id, this.name, this.job, this.photo});
+
+  factory ReviewModel.fromJson(Map<String, dynamic> jsonData) {
+    return ReviewModel(
+        id: jsonData['id'],
+        name: jsonData['name'],
+        job: jsonData['job'],
+        photo: jsonData['photo'],
+        );
   }
 }
 
-class ReviewListModel {
-  final int? status; // 200 - 204 - 400 - 404
-  final List<ReviewListItemModel>? items;
 
-  ReviewListModel({this.status, this.items});
+
+class ReviewListModel {
+  final int? status;  // 200 - 204 - 400 - 404
+  final List<ReviewModel>? items;
+
+  ReviewListModel({this.status,this.items});
 
   factory ReviewListModel.fromJson(Map<String, dynamic> jsonData) {
     return ReviewListModel(
         status: jsonData['status'],
-        items: jsonData['items']
-            .map<ReviewModel>((data) => ReviewModel.fromJson(data))
-            .toList());
+        items:  jsonData['items']
+              .map<ReviewModel>((data) => ReviewModel.fromJson(data))
+              .toList()
+        );
   }
 }
+
